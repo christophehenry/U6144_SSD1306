d80054403c2ea5a412e33d7489114de6896166cf
Christophe Henry <contact@c-henry.fr>
2022-01-09 20:38:25 +0100

    Changes to suite to my needs:
    - Use celsius scale
    - Squash infos into one screen
    - Display "No IPv4 conn." instead of "0.0.0.0" when no IP seems
      detected
    - Formatting for consistency
    - Pad IP line so that provious info won't overlap when IP changes

--- C/ssd1306_i2c.h
+++ C/ssd1306_i2c.h
@@ -10,7 +10,7 @@ extern int i2cd;
 /**********Select display temperature type**************/
 #define CELSIUS       0
 #define FAHRENHEIT    1
-#define TEMPERATURE_TYPE  FAHRENHEIT
+#define TEMPERATURE_TYPE  CELSIUS
 /**********Select display temperature type**************/
 
 /**********Select display network IP type**************/
@@ -23,36 +23,52 @@ extern int i2cd;
 #define IP_DISPLAY_OPEN     0
 #define IP_DISPLAY_CLOSE    1
 #define IP_SWITCH       IP_DISPLAY_OPEN
-#define CUSTOM_DISPLAY   "UCTRONICS"
+#define CUSTOM_DISPLAY   ""
 /************************Turn off the IP display. Can customize the display****************/
 
 #define BLACK 0
 #define WHITE 1
 #define INVERSE 2
 
-#define OLED_CMD  0	                 //command
-#define OLED_DATA 1	                //data
-#define SSD1306_I2C_ADDRESS   0x3C	// 011110+SA0+RW - 0x0C    
+#define OLED_CMD  0                     //command
+#define OLED_DATA 1                    //data
+#define SSD1306_I2C_ADDRESS   0x3C    // 011110+SA0+RW - 0x0C
 #define SSD1306_SWITCHCAPVCC 0x2
 
 #define SSD1306_LCDWIDTH                  128
 #define SSD1306_LCDHEIGHT                 64
+
 void ssd1306_begin(unsigned int switchvcc, unsigned char i2caddr); //switchvcc should be SSD1306_SWITCHCAPVCC
-void OLED_ShowString(unsigned char x,unsigned char y, unsigned char *p,unsigned char Char_Size);
-void OLED_ShowChar(unsigned char x,unsigned char y,unsigned char chr,unsigned char Char_Size);
+void OLED_ShowString(unsigned char x, unsigned char y, unsigned char *p, unsigned char Char_Size);
+
+void OLED_ShowChar(unsigned char x, unsigned char y, unsigned char chr, unsigned char Char_Size);
+
 void OLED_Set_Pos(unsigned char x, unsigned char y);
-void OLED_WR_Byte(unsigned dat,unsigned cmd);  
+
+void OLED_WR_Byte(unsigned dat, unsigned cmd);
+
 void Write_IIC_Data(unsigned char IIC_Data);
+
 void Write_IIC_Command(unsigned char IIC_Command);
-void OLED_DrawBMP(unsigned char x0, unsigned char y0,unsigned char x1, unsigned char y1,unsigned char BMP[][1024],unsigned char);
-void OLED_DrawPartBMP(unsigned char x0, unsigned char y0,unsigned char x1, unsigned char y1,unsigned char BMP[][1024],unsigned char);
+
+void OLED_DrawBMP();
+
+void OLED_DrawPartBMP(unsigned char x0, unsigned char y0, unsigned char x1, unsigned char y1, unsigned char BMP[1024]);
+
 void OLED_Clear(void);
-void OLED_ClearLint(unsigned char,unsigned char);
-void OLED_ShowNum(unsigned char,unsigned char,unsigned int,unsigned char,unsigned char);
-unsigned int oled_pow(unsigned char,unsigned char);
-void LCD_Display(unsigned char symbol);
+
+void OLED_ClearLint(unsigned char, unsigned char);
+
+void OLED_ShowNum(unsigned char, unsigned char, unsigned int, unsigned char, unsigned char);
+
+unsigned int oled_pow(unsigned char, unsigned char);
+
+void LCD_Display();
+
 void LCD_DisPlayCpuSdMemory(void);
-void FirstGetIpAddress(void);
-char* GetIpAddress(void);
+
+void GetIpAddress(void);
+
 unsigned int Obaintemperature(void);
-#endif				/* _SSD1306_I2C_H_ */
+
+#endif                /* _SSD1306_I2C_H_ */
